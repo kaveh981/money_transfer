aac5cbb752d79c4e82c407989fb1cc7f8245986a
Hamid Karimpour <hd.karimpoor@gmail.com>
2016-08-11 19:57:09 -0400

    Add controllers and Routes

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+
+// we'll create our routes here
+
+// get an instance of router
+var router = require('./routes/baseRoute.js');
 
 var app = express();
 
@@ -22,8 +25,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+
+// apply main the routes to our application
+app.use('/', router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/baseRoute.js
@@ -0,0 +1,15 @@
+﻿//In this module we define all base routs such as Admin,Regular User ,CMS or  etc
+
+var express = require('express');
+var router = express.Router();
+var userRoutes = require('./userRoutes.js');
+var adminRoutes = require('./adminRoutes.js');
+
+var baseRouterApp = express();
+
+/* In this middleware we will write all base routes related to website */
+baseRouterApp.use('/', userRoutes);
+baseRouterApp.use('/admin', adminRoutes);
+
+module.exports = baseRouterApp;
+ 
